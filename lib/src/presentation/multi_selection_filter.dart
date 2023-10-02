57392276e595d9941b210a224c00a1fabacf74f7
mihirjsolguruz <mihirj@solguruz.com>
2023-10-02 18:02:34 +0530

    ref: separating methods from main dialog

--- lib/src/presentation/multi_selection_filter.dart
+++ lib/src/presentation/multi_selection_filter.dart
@@ -62,9 +62,12 @@ class _MultiSelectionFilterState extends State<MultiSelectionFilter> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => widget.selectedList.length != widget.textListToShow.length
-          ? ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content:
-                  Text('Item selected list must be same length as Title list')))
+          ? ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                    'Item selected list must be same length as Title list'),
+              ),
+            )
           : buildMultiSelectionDialog(),
       child: widget.child,
     );
@@ -131,7 +134,7 @@ class _MultiSelectionFilterState extends State<MultiSelectionFilter> {
                 const Spacer(),
                 buildRoundCloseButton(
                   iconColor: widget.closeIconColor,
-                  iconBackgroudColor: widget.closeIconBG,
+                  iconBackgroundColor: widget.closeIconBG,
                   onCloseButtonTap: () {
                     Navigator.of(context).pop();
                   },
@@ -149,88 +152,15 @@ class _MultiSelectionFilterState extends State<MultiSelectionFilter> {
                   ),
                   child: Column(
                     children: [
-                      TextField(
-                        controller: searchTextController,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.search,
-                        cursorColor: widget.accentColor,
-                        style: bodyLargeTextStyle(context),
-                        maxLength: TextField.noMaxLength,
-                        decoration: InputDecoration(
-                          counterText: '',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: BorderSide.none),
-                          filled: true,
-                          hintText: widget.searchHint,
-                        ),
-                        onChanged: (v) => onSearched(v),
-                      ),
+                      buildDialogTopSearch(
+                          searchTextController, context, onSearched),
                       verticalSpace(context, 0.02),
-                      Expanded(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: showingFromSearch
-                              ? searchedItemModels.length
-                              : itemModels.length,
-                          itemBuilder: (context, index) {
-                            ItemModel itemModel = showingFromSearch
-                                ? searchedItemModels[index]
-                                : itemModels[index];
-                            return buildCheckboxWithTitle(
-                                context: context,
-                                title: itemModel.itemName,
-                                checkboxValue: itemModel.isSelected,
-                                backgroundColor: widget.checkboxTitleBG,
-                                shadowColor:
-                                    widget.checkboxTitleBG.withOpacity(0.1),
-                                checkboxActiveColor: widget.accentColor,
-                                checkboxCheckColor: widget.checkboxCheckColor,
-                                textColor: widget.checkboxTitleTextColor,
-                                onCheckboxChanged: (isSelected) {
-                                  setState(
-                                    () => onItemSelected(
-                                        itemModel.id, isSelected),
-                                  );
-                                },
-                                onTap: () {
-                                  setState(
-                                    () => onItemSelected(
-                                        itemModel.id, !itemModel.isSelected),
-                                  );
-                                });
-                          },
-                        ),
-                      ),
+                      buildDialogList(showingFromSearch, searchedItemModels,
+                          itemModels, setState, onItemSelected),
                       verticalSpace(context, 0.02),
                       if (SizeUtil.bottom == 0 && widget.showChips)
-                        Container(
-                          height: MediaQuery.of(context).size.longestSide * 0.2,
-                          alignment: Alignment.center,
-                          child: itemModels.isNotEmpty
-                              ? SingleChildScrollView(
-                                  child: buildSelectedListChip(
-                                    context: context,
-                                    chipBackgroundColor: widget.checkboxTitleBG,
-                                    closeIconColor: widget.accentColor,
-                                    textColor: widget.checkboxTitleTextColor,
-                                    checkedListForChip: itemModels
-                                        .where((element) => element.isSelected)
-                                        .toList(),
-                                    onChipDeleted: (selectedModel) {
-                                      setState(
-                                        () {
-                                          onItemSelected(
-                                              selectedModel.id,
-                                              !itemModels[selectedModel.id]
-                                                  .isSelected);
-                                        },
-                                      );
-                                    },
-                                  ),
-                                )
-                              : const SizedBox.shrink(),
-                        ),
+                        buildDialogChips(
+                            context, itemModels, setState, onItemSelected),
                       verticalSpace(
                           context,
                           MediaQuery.of(context).viewInsets.bottom == 0
@@ -253,6 +183,96 @@ class _MultiSelectionFilterState extends State<MultiSelectionFilter> {
           );
         });
       },
+    );
+  }
+
+  TextField buildDialogTopSearch(TextEditingController searchTextController,
+      BuildContext context, void Function(String value) onSearched) {
+    return TextField(
+      controller: searchTextController,
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.search,
+      cursorColor: widget.accentColor,
+      style: bodyLargeTextStyle(context),
+      maxLength: TextField.noMaxLength,
+      decoration: InputDecoration(
+        counterText: '',
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none),
+        filled: true,
+        hintText: widget.searchHint,
+      ),
+      onChanged: (v) => onSearched(v),
+    );
+  }
+
+  Widget buildDialogList(
+      bool showingFromSearch,
+      List<ItemModel> searchedItemModels,
+      List<ItemModel> itemModels,
+      StateSetter setState,
+      void Function(int id, bool isSelected) onItemSelected) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount:
+            showingFromSearch ? searchedItemModels.length : itemModels.length,
+        itemBuilder: (context, index) {
+          ItemModel itemModel =
+              showingFromSearch ? searchedItemModels[index] : itemModels[index];
+          return buildCheckboxWithTitle(
+              context: context,
+              title: itemModel.itemName,
+              checkboxValue: itemModel.isSelected,
+              backgroundColor: widget.checkboxTitleBG,
+              shadowColor: widget.checkboxTitleBG.withOpacity(0.1),
+              checkboxActiveColor: widget.accentColor,
+              checkboxCheckColor: widget.checkboxCheckColor,
+              textColor: widget.checkboxTitleTextColor,
+              onCheckboxChanged: (isSelected) {
+                setState(
+                  () => onItemSelected(itemModel.id, isSelected),
+                );
+              },
+              onTap: () {
+                setState(
+                  () => onItemSelected(itemModel.id, !itemModel.isSelected),
+                );
+              });
+        },
+      ),
+    );
+  }
+
+  Widget buildDialogChips(
+      BuildContext context,
+      List<ItemModel> itemModels,
+      StateSetter setState,
+      void Function(int id, bool isSelected) onItemSelected) {
+    return Container(
+      height: MediaQuery.of(context).size.longestSide * 0.2,
+      alignment: Alignment.center,
+      child: itemModels.isNotEmpty
+          ? SingleChildScrollView(
+              child: buildSelectedListChip(
+                context: context,
+                chipBackgroundColor: widget.checkboxTitleBG,
+                closeIconColor: widget.accentColor,
+                textColor: widget.checkboxTitleTextColor,
+                checkedListForChip:
+                    itemModels.where((element) => element.isSelected).toList(),
+                onChipDeleted: (selectedModel) {
+                  setState(
+                    () {
+                      onItemSelected(selectedModel.id,
+                          !itemModels[selectedModel.id].isSelected);
+                    },
+                  );
+                },
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
