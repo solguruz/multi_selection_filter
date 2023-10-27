404836ec225bce40d1a42e25f06fc46630735bf7
mihirjsolguruz <mihirj@solguruz.com>
2023-10-27 16:14:34 +0530

    feat: adding more dart documentation comments

--- lib/src/utils/size_util.dart
+++ lib/src/utils/size_util.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// Use this class to perform common size-related tasks, such as converting between
+/// different units (e.g., pixels and points), scaling sizes based on the device's
+/// screen density, and more.
 class SizeUtil {
   static late MediaQueryData _mediaQuery;
   static late double screenWidth;
@@ -8,6 +11,7 @@ class SizeUtil {
   static late double bottom;
   static late Orientation orientation;
 
+  /// Initializes SizeUtil class
   void init(BuildContext context) {
     _mediaQuery = MediaQuery.of(context);
     screenWidth = _mediaQuery.size.width;
@@ -16,11 +20,13 @@ class SizeUtil {
     bottom = _mediaQuery.viewInsets.bottom;
   }
 
+  /// get dynamic height
   static double sizeHeight(double inputHeight) {
     double screenHeight = SizeUtil.screenHeight;
     return (inputHeight / 812.0) * screenHeight;
   }
 
+  /// get dynamic width
   static double sizeWidth(double inputWidth) {
     double screenWidth = SizeUtil.screenWidth;
     return (inputWidth / 375.0) * screenWidth;
